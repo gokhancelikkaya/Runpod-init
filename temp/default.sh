f2c6a97406db3a0fe9b1597ff7fc468cdd3d13b1
Tom <mushroomfleet@gmail.com>
2024-10-15 19:27:07 +0100

    Update default.sh

--- temp/default.sh
+++ temp/default.sh
@@ -86,7 +86,7 @@ ULTRALYTICS_MODELS=(
 )
 
 SAMS_MODELS=(
-    "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/detection/bbox/face_yolov8m.pt"
+    "https://huggingface.co/datasets/Gourieff/ReActor/blob/main/models/sams/sam_vit_b_01ec64.pth"
 )
 
 CONTROLNET_MODELS=(
